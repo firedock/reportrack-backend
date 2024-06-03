60480ea14621175019df66639745ee658d778af9
Robert Flanagan <robert@firedock.com>
2024-06-03 11:58:48 -0700

    update

--- config/database.js
+++ config/database.js
@@ -56,22 +56,22 @@ module.exports = ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env(
+          'DATABASE_HOST',
+          'reportrack-db-1.c6mzqt23cxdm.us-west-1.rds.amazonaws.com'
+        ),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
-        },
+        database: env('DATABASE_NAME', 'reportrack'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'rfacc355'),
+        ssl: env.bool('DATABASE_SSL', true)
+          ? {
+              rejectUnauthorized: env.bool(
+                'DATABASE_SSL_REJECT_UNAUTHORIZED',
+                false
+              ),
+            }
+          : false,
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: {
