5610d82764ad3845645f2bace2daae534246cf72
Robert Flanagan <robert@firedock.com>
2025-04-28 23:32:34 -0700

    provider-upload-aws-s3

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,23 @@
 module.exports = [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://market-assets.strapi.io',
+            'https://reportrack.s3.us-west-1.amazonaws.com', // ✅ Correct: allow your S3 bucket
+          ],
+        },
+      },
+    },
+  },
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.js
+++ config/plugins.js
@@ -7,13 +7,29 @@ module.exports = ({ env }) => ({
         port: env.int('SMTP_PORT', 587),
         auth: {
           user: env('SMTP_USERNAME', 'noreply@reportrack.com'),
-          pass: env('SMTP_PASSWORD', '$sfuciNuXM9Kk4v!'),
+          pass: env('SMTP_PASSWORD'),
         },
         tls: { ciphers: 'SSLv3' }, // Add TLS configuration
       },
       settings: {
         defaultFrom: 'noreply@reportrack.com',
         defaultReplyTo: 'noreply@reportrack.com',
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        baseUrl: `https://${env('AWS_BUCKET_NAME')}.s3.${env(
+          'AWS_REGION'
+        )}.amazonaws.com`, // ✅ Add this!
+        params: {
+          Bucket: env('AWS_BUCKET_NAME'),
+        },
       },
     },
   },
