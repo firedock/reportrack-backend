e22293a3ab415813ba66ea16755c1d7dfd997783
Robert Flanagan <robert@firedock.com>
2025-07-15 00:16:27 -0700

    prevent service record dupe

--- src/api/service-record/content-types/service-record/lifecycles.js
+++ src/api/service-record/content-types/service-record/lifecycles.js
@@ -2,7 +2,31 @@
 
 module.exports = {
   async beforeCreate(event) {
-    // console.log('beforeCreate service-record');
+    const { data } = event.params;
+    
+    if (data.users_permissions_user) {
+      const existingActiveRecords = await strapi.entityService.findMany('api::service-record.service-record', {
+        filters: {
+          users_permissions_user: data.users_permissions_user,
+          endDateTime: { $null: true }
+        },
+        populate: ['users_permissions_user']
+      });
+
+      if (existingActiveRecords && existingActiveRecords.length > 0) {
+        const activeRecord = existingActiveRecords[0];
+        const error = new Error(`User already has an active service record (ID: ${activeRecord.id}) started at ${activeRecord.startDateTime}. Please complete the existing service record before starting a new one.`);
+        error.name = 'ValidationError';
+        Object.assign(error, {
+          details: {
+            code: 'ACTIVE_SERVICE_RECORD_EXISTS',
+            activeRecordId: activeRecord.id,
+            startDateTime: activeRecord.startDateTime
+          }
+        });
+        throw error;
+      }
+    }
   },
   async beforeUpdate(event) {
     // console.log('beforeUpdate service-record');
